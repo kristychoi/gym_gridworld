d3ad434a7a6671e2b48bd4c03929cecb82164045
sudarshanseshadri <sudarshadri@gmail.com>
2018-02-12 22:41:27 -0800

    fixes and simplification of OneHot. GridWorldEnv restructured

--- gym_gridworld/envs/gridworld.py
+++ gym_gridworld/envs/gridworld.py
@@ -12,25 +12,27 @@ class GridWorldEnv(gym.Env):
 	def __init__(self):
 		# specifying size, start_state, terminal_state_d as per OG code
 		self.start = (0,0)
-		self.terminal_d = {(1, 1):1, (3, 3):10}
+
+		# state space is a 4x4 grid
+		self.size = 4
 
 		# action space of 4 discrete actions
 		self.action_space = spaces.Discrete(4)
 
 		# make sure start state is within grid world boundaries
 		assert self.in_bounds(self.start)
-		self.terminal_dict = {}
+		
+		self.terminal_d = {(1, 1):1, (3, 3):10}
+		# need at least one terminal state. And they all must be in bounds
 		assert len(self.terminal_d.keys()) > 0
-
 		for state in self.terminal_d:
 			assert self.in_bounds(state)
 
-		self.state = self.start
-		self.violation = False
-		self.done = (self.state in self.terminal_d)
-		assert not self.done
-		
-		self.penalty = -0.1
+		# default reward when not moving from a terminal state
+		self.default_reward = -0.1
+
+		self._reset()
+
 		# get seed
 		self._seed()
 
@@ -56,7 +58,7 @@ class GridWorldEnv(gym.Env):
 		assert not self.done
 		state = self.state
 
-		# take specific actions 
+		# take specific actions to calculate prospective next state
 		if action == 0: #right
 			next_state = (state[0], state[1]+1)
 		elif action == 1: #up
@@ -65,29 +67,25 @@ class GridWorldEnv(gym.Env):
 			next_state = (state[0], state[1]-1)
 		else: #down
 			next_state = (state[0]+1, state[1])
-		# if next state is out of bounds, incurs a penalty. stay in state
+		
+		# if next state is in bounds, update state
 		if self.in_bounds(next_state):
 			self.state = next_state
-		else:
-			self.violation = True	
-		# doesn't return anything, but sets current state to next state
-
+			# can't move out of terminal state. 
+			if self.state in self.terminal_d:
+				self.done = True
 
 	def _get_reward(self):
 		"""
 		this returns the reward that you would get from taking a certain action
 		"""
 		if self.state in self.terminal_d:
-			self.done = True
 			return self.terminal_d[self.state]
-		elif self.violation:
-			self.violation = False  # set this to False now
-			return self.penalty
 		else:
-			return 0  # not sure if you're actually supposed to return 0
+			return self.default_reward
 
 	def in_bounds(self, state):
-		return (0 <= state[0] < self.action_space.n) and (0 <= state[1] < self.action_space.n)
+		return (0 <= state[0] < self.size) and (0 <= state[1] < self.size)
 
 	def get_state(self):
 		return self.state
@@ -97,8 +95,10 @@ class GridWorldEnv(gym.Env):
 
 	def _reset(self):
 		self.state = self.start
-		self.done = False
+		self.done = (self.state in self.terminal_d)
+		assert not self.done
 		return self.get_state()
 
 	def _render(self, mode='human', close=False):
-		raise NotImplementedError
+		# raise NotImplementedError
+		return
--- gym_gridworld/envs/onehot.py
+++ gym_gridworld/envs/onehot.py
@@ -6,91 +6,18 @@ import gym
 import numpy as np
 from gym import error, spaces, utils
 from gym.utils import seeding
+from gridworld import GridWorldEnv 
 
-
-class OneHotGridWorldEnv(gym.Env):
+class OneHotGridWorldEnv(GridWorldEnv):
 	metadata = {'render.modes': ['human']}
-
-	def __init__(self):
-		self.start = 0
-		self.terminal_d = {5: 1, 15: 10}  # key indexes position of 1
-
-		# action space of 4 discrete actions
-		self.action_space = spaces.Discrete(4)
-
-		self.state = self.start
-		self.violation = False
-		self.done = False
-		self.penalty = -0.1
-		# set seed
-		self._seed()
-
-	def _seed(self, seed=None):
-		self.np_random, seed = seeding.np_random(seed)
-		return [seed]
-
-	def _step(self, action):
-		"""
-		Takes a step in given environment; returns obs, reward, done, info
-		"""
-		self._take_action(action)
-		reward = self._get_reward()
-		obs = self.get_state()
-		episode_over = self.get_status()
-
-		return obs, reward, episode_over, {}
-
-	def _take_action(self, action):
-		"""
-		take given action <action>, then update next state if doesn't go past
-		boundaries of given grid world
-		"""
-		assert not self.done
-		state = self.state
-
-		if action == 0: # right
-			next_state = state + 1
-		elif action == 1: # up
-			next_state = state - 4
-		elif action == 2: # left
-			next_state = state - 1
-		else: # down
-			next_state = state + 4
-		# if next_state is out of bounds, incurs a penalty and stays in current state
-		if 0 <= next_state <= 15:
-			self.state = next_state
-		else:
-			self.violation = True
-
-	def _get_reward(self):
-		"""
-		returns reward for resulting state after taking an action
-		"""
-		if self.state in self.terminal_d:
-			self.done = True
-			return self.terminal_d[self.state]
-		elif self.violation:
-			self.violation = False
-			return self.penalty
-		else:
-			return 0
 
 	def get_state(self):
 		"""
 		takes the position of the 1 (self.state) and turns this
 		into a one-hot encoding representation of the current state
 		"""
-		state_vec = np.zeros(16)
-		state_vec[self.state] = 1
+		row, col = self.state
+		state_vec = np.zeros(self.size**2)
+		idx = self.size*row + col
+		state_vec[idx] = 1
 		return state_vec
-
-	def get_status(self):
-		return self.done
-
-	def _reset(self):
-		self.state = self.start
-		self.done = False
-		return self.get_state()
-
-	def _render(self, mode='human', close=False):
-		raise NotImplementedError
